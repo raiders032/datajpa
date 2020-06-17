3f25c50e4c420551743fa826b6616b3043f524a4
raiders032 <nameks@naver.com>
2020-06-17 11:56:45 +0900

    컬렉션 파라미터 바인딩

--- src/main/java/com/youngthree/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/youngthree/study/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
@@ -20,5 +21,8 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query("select new com.youngthree.study.datajpa.dto.MemberDto(m.id, m.username,t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByName(@Param("names")Collection<String> names);
 
 }
